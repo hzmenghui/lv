2d0f61dfa3f41b6311559f0814cf9215bbc912bb
hzmenghui <1690611599@qq.com>
2017-04-24 15:53:55 +0800

    jiben

---
+++ module/Application/src/Controller/PhotoController.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Xiahuili
+ * Date: 2017/4/24
+ * Time: 13:57
+ */
+class photo extends \Zend\Mvc\Controller\AbstractActionController{
+    public function photo(){
+        echo "I'm a photo";
+}
+}
